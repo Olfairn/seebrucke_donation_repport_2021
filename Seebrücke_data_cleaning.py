28ab1772709bf9d23ad7288c0d09e087f7172fc8
Olfairn <flo.marandet@gmail.com>
2021-09-12 14:11:19 +0200

    improve data transfert quality and add verein csv

--- Seebrücke_data_cleaning.py
+++ Seebrücke_data_cleaning.py
@@ -1,4 +1,5 @@
 
+#TODO Improve Transfert files
 #%%
 from numpy import NaN
 import pandas as pd
@@ -18,12 +19,10 @@ twingle3 = pd.read_csv('data/01_c_twingle_Seebücke_2020_2.csv',sep=';')
 twingle_dauer = pd.read_csv('data/02_twingle_Dauerspende.csv',sep=';')
 twingle_Winter = pd.read_csv('data/03_twingle_Winterkampagne.csv',sep=';')
 twingle_united = pd.read_csv('data/04_twingle_UnitedWeShare.csv',sep=';')
-#TODO Check if Fundbox data contains everythin
-Fundbox_2020 = pd.read_csv('data/FundraisingBox_Donations_2020_non_anonymized.csv',sep=';',encoding='latin1')
-
+Fundbox = pd.read_csv('data/NEW_ FundraisingBox_Donations_2021-07-28.csv',sep=';',encoding='latin1')
 Transfert_1 = pd.read_excel('data/Kontoauszüge Seebrücke 2020.xlsx') #Everything is incoming
 Transfert_2 = pd.read_excel('data/Kontoauszüge 2018-2019.xlsx',header=1) #INcoming => +
-Transfert_3 = pd.read_excel('data/new_konto_2020.xlsx',parse_dates=True) #INcoming => +
+Transfert_3 = pd.read_excel('data/new_konto_2020.xlsx',parse_dates=True) #'Soll/Haben' => H
 
 #%%
 # Select usefull twingle data
@@ -47,16 +46,12 @@ twingle_df['donated_at'] = to_datetime(twingle_df['donated_at'])
 
 #%%
 # Select usefull Fundbox data
-Fundbox_df = Fundbox_2020[['donated_at','amount','by_recurring','interval','city','country','email_address','company_name']]
+Fundbox_df = Fundbox[['donated_at','amount','by_recurring','interval','city','country','email_address','company_name']]
 Fundbox_df['donated_at'] = to_datetime(Fundbox_df['donated_at']).copy()
+Fundbox_df['by_recurring'].replace(0,1,inplace=True) #reccuring are 0
 Fundbox_df['by_recurring'] = Fundbox_df['by_recurring'].fillna(0) #Unlike Twingle, non reccuring are nulls
 
 #%%
-#Considering that only 15 person transferred money more than once, for simplicity, I will count every transfert as a one time donation
-# For the same reason, I ignored the name col and didn't try to match it with the other datasets
-Transfert_1['Kontoinhaber*in'].value_counts()
-#%%
-
 #Get rid of outgoing transfert  
 Transfert_2 = Transfert_2.loc[Transfert_2[' + '] > 0].copy()
 Transfert_3 = Transfert_3.loc[Transfert_3['Soll/Haben'] == 'H'].copy()
@@ -76,24 +71,38 @@ Transfert_1.columns = ['donated_at','amount','company_name']
 Transfert_3.columns = ['donated_at','address','company_name','currency','amount','in/out']
 Transfert_3 = Transfert_3[['donated_at','amount','company_name']]
 
-
 #%%
 Transfert_df = pd.concat([Transfert_1,Transfert_2,Transfert_3],ignore_index=True)
 Transfert_df['donated_at'] = to_datetime(Transfert_df['donated_at'],errors='coerce')
-Transfert_df = Transfert_df.dropna(subset=['donated_at'])
-
-Transfert_df['by_recurring'] = 0
-Transfert_df['email_address'] = random.sample(range(1,10000),len(Transfert_df))
+Transfert_df = Transfert_df.dropna(subset=['donated_at']) #very dirty data
 
 #%%
-#* Drop verein donations
-verein_index = Transfert_df[Transfert_df['company_name'].str.contains('e\. V',flags=re.IGNORECASE)==True].index
+#Transfert contains about 200 duplicates 
+Transfert_df.drop_duplicates(inplace=True)
+
+
+#%%
+#Considering that only a few people transferred money more than once, for simplicity, I will count every transfert as a one time donation
+# For the same reason, I ignored the name col and didn't try to match it with the other datasets
+Transfert_df['company_name'].value_counts()
+#%%
+Transfert_df['by_recurring'] = 0
+Transfert_df['email_address'] = random.sample(range(1,1000000),len(Transfert_df))
+#%%
+#* Drop verein grants
+verein = Transfert_df[Transfert_df['company_name'].str.contains('e\. V|stiftung',flags=re.IGNORECASE)==True]
+
+verein = verein.loc[verein['amount'] > 2000].copy() #under 2000 these are donations and not grants
+verein_index = verein.index
+
 Transfert_df.drop(verein_index,inplace=True)
 
 #%%
 #Keep just companies in the company col
 Transfert_df['company_name'] = Transfert_df.company_name[Transfert_df['company_name'].str.contains('gmbh',flags=re.IGNORECASE)==True]
 
+#%%
+Transfert_df
 #%%
 #Merge Twingle and Fundbox data
 df = pd.concat([twingle_df,Fundbox_df,Transfert_df],ignore_index=True)
@@ -102,6 +111,28 @@ df = pd.concat([twingle_df,Fundbox_df,Transfert_df],ignore_index=True)
 #Clean city col
 df['city'] = df['city'].str.strip()
 df['city'] = df['city'].str.lower()
+
+berlin_to_replace = ['berlin - kreuzberg', 'berlin - neuköln', 'berlin - kreuzberg', 'berlin wedding', 'berlin - friedrichshain',
+                      'mitte', 'berlin - mitte', 'berlin - prenzlauer berg', 'belrin', 'berlim']
+df['city'].replace(berlin_to_replace, 'berlin', inplace=True)
+
+frankfurt_to_replace =['frankfurt', 'frankfurt main', 'frankfurt m', 'frankfurt a. main']
+df['city'].replace(frankfurt_to_replace, 'frankfurt am main', inplace=True)
+
+df['city'].replace('freiburg i. br', 'freiburg', inplace=True)
+
+munich_to_replace = ['munich', 'münchen (glockenbachviertel)', 'muenchen', 'garching b. münchen']
+df['city'].replace(munich_to_replace, 'münchen', inplace=True)
+
+cologne_to_replace = ['köln - ehrenfeld', 'cologne', 'koln']
+df['city'].replace(cologne_to_replace, 'köln', inplace=True)
+
+hamburg_to_replace = ['hamurg', 'eimsbüttel - hamburg rotherbaum', 'hambrug', 'hanburg']
+df['city'].replace(hamburg_to_replace, 'hamburg', inplace=True)
+
+rest_to_replace = {'nurnberg': 'nürnberg', 'duesseldorf': 'düsseldorf', 'bochum - bochum-süd': 'bochum',
+                  'rheinland-pfalz - mainz': 'mainz', 'inglostadt': 'ingolstadt'}
+df['city'].replace(rest_to_replace, inplace=True)
 
 #%%
 #Factorise email to annonymize the dataset and delete email col 
@@ -113,41 +144,20 @@ df.drop('email_address',axis=1,inplace=True)
 #%%
 df['company_name'] = df['company_name'].notnull().astype('int')
 df.rename(columns={'company_name':'company'})
-#%%
-df.to_csv('data_seebrücke_anno.csv')
-
-
 
 #%%
-#! Fundbox gives person_id
-Fundbox1 = pd.read_csv('data/05_FundraisingBox_Seebruecke_Spende.csv',sep=';')
-Fundbox2 = pd.read_csv('data/06_FundraisingBox_Seebuecke_donations.csv',sep=';')
-Fundbox3 = pd.read_csv('data/07_FundraisingBox_Seebruecke_foerdern_Newsletter.csv',sep=';')
-Fundbox4 = pd.read_csv('data/08_FundraisingBox_Seebruecke_Winterkampagne.csv',sep=';')
-Fundbox5 = pd.read_csv('data/09_FundraisingBox_Seebruecke_winter_donation_campaign.csv',sep=';')
-#! Strange one
-#Fundbox6 = pd.read_csv('data/10_FundraisingBox_Seebruecke_foerdern_Social_Media.csv',sep=';',encoding='cp1252')
-Fundbox7 = pd.read_csv('data/11_FundraisingBox_Seebruecke_foerdern_der_freitag.csv',sep=';')
+verein['company_name'] = verein['company_name'].notnull().astype('int')
+verein.rename(columns={'company_name':'verein'})
+#%%
+#add Geo data
+geo = pd.read_csv("map_de.csv")
+geo['city'] = geo['city'].str.lower()
+df = df.merge(geo[['city','lat','lng']], how='left')
+
+
+#%%
+df.to_csv('seebrücke_donations_anonymized.csv')
+
+df.to_csv('seebrücke_verein_anonymized.csv')
+
 # %%
-#Transfert_df_5000 = Transfert_df.loc[Transfert_df['amount'] >= 5000]
-#Transfert_df_5000
-
-fund_df = pd.concat([Fundbox1,Fundbox2,Fundbox3,Fundbox4,Fundbox5,Fundbox7])
-
-#%%
-fund_df.amount.sum()
-#%%
-Fundbox_df.amount.sum()
-#%%
-
-small_fund = fund_df[['donated_at','amount']]
-
-#%%
-small_fund.donated_at = to_datetime(small_fund.donated_at)
-#%%
-small_fund.donated_at.hist()
-pl.suptitle("Old Fundbox")
-#%%
-Fundbox_df.donated_at.hist()
-pl.suptitle("New Fundbox Data")
-
